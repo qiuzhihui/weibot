534487eab6747dd180f1ec25d600493d1a4d2429
Zhihui Qiu <zhihui.qiu.tufts@gmail.com>
2017-09-24 11:49:43 -0700

    Frontend skeleton

--- Application/views.py
+++ Application/views.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
-app = Flask('weibot')
+app = Flask('weibot', static_folder='Application/static/dist', template_folder='Application/static')
 
 # index page
 @app.route("/")
 def show_index_page():
-    return 'haha'
+    return render_template('index.html')
