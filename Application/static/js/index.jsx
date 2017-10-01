3b3346f8d10b065a28f35b25b15abb512cdf5e0a
Zhihui Qiu <zhihui.qiu.tufts@gmail.com>
2017-10-01 00:36:28 -0700

    Implemented simple to do list

--- Application/static/js/index.jsx
+++ Application/static/js/index.jsx
@@ -2,14 +2,17 @@
 import React from "react";
 import { render } from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import todoApp from './reducers';
-import App from "./App";
+import App from "./components/App";
 
 
 
 let store = createStore(todoApp)
 
 render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById("content")
 );
