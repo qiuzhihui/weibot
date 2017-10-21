247261df96d4d9cfa2c50042b8d52ca07f71244e
Zhihui Qiu <zhihui.qiu.tufts@gmail.com>
2017-10-21 10:44:22 -0700

    Added footer

--- Application/static/js/components/Footer.js
+++ Application/static/js/components/Footer.js
@@ -1,23 +1,44 @@
 import React from 'react'
-import {Footer} from 'react-materialize'
 
 const AppFooter = () => (
-  <div className='foot'>
-    <Footer copyrights="&copy 2015 Copyright Text"
-      links={
-        <ul>
-          <li><a className="grey-text text-lighten-3" href="#!">Link 1</a></li>
-          <li><a className="grey-text text-lighten-3" href="#!">Link 2</a></li>
-          <li><a className="grey-text text-lighten-3" href="#!">Link 3</a></li>
-          <li><a className="grey-text text-lighten-3" href="#!">Link 4</a></li>
-        </ul>
-      }
-      className='footer'
-    >
-        <h5 className="white-text">Footer Content</h5>
-        <p className="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-    </Footer>
-  </div>
+    <footer>
+      <div className="container">
+        <div className="row" >
+            <div className="col-lg-12">
+                <h6 id="techword">Follow us on:</h6>
+                <ul className="list-inline">
+                    <li>
+                        <img className="social-icon" id="twitter-icon" src="static/img/twitter.svg" /><a href="https://www.python.org/">Twitter</a>
+                    </li>
+                    <li className="footer-menu-divider">&sdot;</li>
+                    <li>
+                        <img className="social-icon" id="facebook-icon" src="static/img/facebook.jpeg" /><a href="https://www.docker.com/">  Facebook</a>
+                    </li>
+                    <li className="footer-menu-divider">&sdot;</li>
+                    <li>
+                        <img className="social-icon" id="yelp-icon" src="../static/img/Yelp.svg" /><a href="https://www.yelp.com/developers/documentation/v2/overview">  Yelp </a>
+                    </li>
+                    <li className="footer-menu-divider">&sdot;</li>
+                    <li>
+                        <img className="social-icon" id="instagram-icon" src="../static/img/instagram.png" /><a id="bootstraplink" href="http://getbootstrap.com/">Instagram </a>
+                    </li>
+                </ul>
+                <ul className="list-inline">
+                    <li>
+                <p className="copyright text"> Uboston Auto School</p>
+                        </li>
+                    <li className="footer-menu-divider">&sdot;</li>
+                    <li>
+                    <p id="author" className="copyright text-muted small">Created By Jumbos</p>
+                    </li>
+                    <li className="footer-menu-divider">&sdot;</li>
+                    <li><p className="copyright text-muted small">
+                        Copyright Aug. 2017</p></li>
+                </ul>
+            </div>
+        </div>
+      </div>
+    </footer>
 )
 
 export default AppFooter
