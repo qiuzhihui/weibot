517fe36a47c4f305541609a572a689391a7f570a
maple7sha <wanhao.maple@gmail.com>
2017-10-21 18:15:02 -0700

    update videolayout center margin

--- Application/static/js/components/VideoOverlay.js
+++ Application/static/js/components/VideoOverlay.js
@@ -2,14 +2,15 @@ import React from 'react'
 import {PageHeader, Grid, Col, Row, Image} from 'react-bootstrap';
 
 const style = {
-  'position': 'absolute',
-  'zIndex': 1,
-  'padding':'30px',
-  'color': 'white',
+  position: 'absolute',
+  zIndex: 1,
+  padding:'30px',
+  color: 'white',
   position: 'absolute',
   textAlign: 'center',
-  width: '100%'
-  }
+  width: '60%',
+  marginLeft: '20%',
+}
 
 
 const styleIcon = {
@@ -32,22 +33,21 @@ class VideoOverlay extends React.Component {
         <h2>Uboston Auto School</h2>
         <hr className="intro-divider" style={{display: 'block'}}/>
         <div style={styleIcon}>
-          <Grid>
-            <Row>
-              <Col xs={4} md={4}>
-                <Image className="VideoOverlayIcon" src="static/img/repair.png" circle />
-                <p> repair </p>
-              </Col>
-              <Col xs={4} md={4}>
-                <Image className="VideoOverlayIcon" src="static/img/rent.png" circle />
-                <p> Rent </p>
-              </Col>
-              <Col xs={4} md={4}>
-                <Image className="VideoOverlayIcon" src="static/img/used.png" circle />
-                <p> Sell </p>
-              </Col>
-            </Row>
-          </Grid>
+          <Row>
+            <Col xs={4} md={4}>
+              <Image className="VideoOverlayIcon" src="static/img/repair.png" circle 
+                     onClick={ () => {location.hash = "#id3"} }/>
+              <p> repair </p>
+            </Col>
+            <Col xs={4} md={4}>
+              <Image className="VideoOverlayIcon" src="static/img/rent.png" circle id="id3"/>
+              <p> Rent </p>
+            </Col>
+            <Col xs={4} md={4}>
+              <Image className="VideoOverlayIcon" src="static/img/used.png" circle />
+              <p> Sell </p>
+            </Col>
+          </Row>
         </div>
       </div>
     )
