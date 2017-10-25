bc254d026c0afbb2d6ae04770f6e5193b042c162
hyperchi <hyperbolechi@gmail.com>
2017-10-24 23:38:19 -0400

    add page

--- Application/views.py
+++ Application/views.py
@@ -8,3 +8,11 @@ app = Flask('weibot', static_folder='Application/static', template_folder='Appli
 def show_index_page():
     return render_template('index.html')
 
+
+@app.route("/rental/")
+def show_rental_page():
+    return render_template('rental.html')
+
+@app.route("/used/")
+def show_used_page():
+    return render_template('used.html')
