2f0a22f898e262d7a6d3d82286f8ab375a14de94
maple7sha <wanhao.maple@gmail.com>
2017-11-11 16:26:11 -0800

    Adds car brand logos as requested

--- Application/static/js/components/BrandGrid.js
+++ Application/static/js/components/BrandGrid.js
@@ -6,6 +6,13 @@ const BrandGrid = () => (
   <div style={{ width: "100%"}}>
     <Grid >
       <Row className="show-grid" style={{height:"200px", padding: "10px"}}>
+
+        <Col xs={6} md={3}>
+          <img src="/static/img/brands/Jaguar.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
+        </Col>
+        <Col xs={6} md={3}>
+          <img src="/static/img/brands/Lexus.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
+        </Col>
         <Col xs={6} md={3}>
           <img src="/static/img/brands/Audi.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
         </Col>
@@ -13,10 +20,16 @@ const BrandGrid = () => (
           <img src="/static/img/brands/Benz.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
         </Col>
         <Col xs={6} md={3}>
+          <img src="/static/img/brands/Porsche.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
+        </Col>
+        <Col xs={6} md={3}>
           <img src="/static/img/brands/LandRover.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
         </Col>
         <Col xs={6} md={3}>
-          <img src="/static/img/brands/Lexus.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
+          <img src="/static/img/brands/Vw.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
+        </Col>
+        <Col xs={6} md={3}>
+          <img src="/static/img/brands/Bmw.png" width= "100%"  style={{display:"block", margin:"0 auto"}}></img>
         </Col>
       </Row>
     </Grid>
